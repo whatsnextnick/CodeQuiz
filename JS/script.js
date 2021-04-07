f6d385e33046a76884b1d90883c22190a2a4dcf4
Jawascript <n.jmonplaisir@gmail.com>
2021-04-07 05:53:42 -0400

    final updates - question handler and scores

--- JS/script.js
+++ JS/script.js
@@ -58,3 +58,141 @@ $(document).ready(function() {
         }
       })
     }
+      
+    // on li click, go to next question or display score
+    $('#question-page').click(function() {
+        if (event.target.matches('li')) {
+          var lastQuestion = questions.length-1;
+    
+          if (currentQuestion == lastQuestion) {
+            handleQuestion(event.target.innerText);
+    
+            quizDone = true; // the user is done
+            handleScores();
+          }
+    
+          else {
+            handleQuestion(event.target.innerText)
+    
+            currentQuestion++ // next question
+            renderQuestion();
+          }
+        }
+      })
+    
+    
+      // generate questuion
+      function renderQuestion() {
+        var question = questions[currentQuestion];
+    
+        $('#question-page').show() // show question page
+    
+        $('#question').text(question.title) // add value to the title
+    
+        $('#responses').html(''); // delete all the li
+    
+        var responses = question.choices;
+        responses.forEach((text, index) => {
+          var li = document.createElement('li');
+    
+          li.innerText = text; // add some text to the li
+          $('#responses').append(li);
+        });
+      }
+    
+    
+      // handles if user selection is right or wrong
+      function handleQuestion(selection) {
+        var answer = questions[currentQuestion].answer;
+    
+        if (selection == answer) {
+          score++
+          showAlert('Right', 400);
+        }
+    
+        else {
+          showAlert('Wrong', 400);
+        }
+      }
+    
+    
+      function setTimer() {
+        var sec = 20;
+        var interval = setInterval(function functionName() {
+          sec--
+          $('#timer').html(sec);
+    
+          if (sec == 0) {
+            clearInterval(interval);
+            handleScores();
+    
+            showAlert('Time is over!', 1200, 'red');
+          }
+    
+          if (quizDone) {
+            clearInterval(interval);
+          }
+        }, 1000)
+      }
+    
+    
+      function showAlert(text, time, bcolor) {
+        $('#alert').show();
+        $('#alert').text(text);
+    
+        // timer done show background in red
+        $('#alert').css('background-color', bcolor);
+    
+        setTimeout(function() {
+          $('#alert').hide();
+        }, time);
+      }
+    
+      $('#view-scores').click(function() {
+        // $('#view-scores').html('');
+    
+        populateScores();
+        handleScreens();
+      }) // on button click show scores
+    
+      var localRecords = JSON.parse(localStorage.getItem('records'));
+      var records;
+    
+      if (localRecords) {
+        records = localRecords;
+      }
+    
+      else {
+        records = [];
+      }
+    
+      function handleScores() {
+        var record = {};
+        record.user = currentUser;
+        record.result = score;
+        records.push(record);
+    
+        localStorage.setItem('records', JSON.stringify(records)); // save all the information to local storage
+    
+        // screens
+        handleScreens();
+    
+        $('#scores-list').html(''); // erase all previous scores
+    
+        populateScores();
+      }
+    
+      function populateScores() {
+        $('#scores-list').empty();
+    
+        records.forEach(function(object) {
+          $('#scores-list').append("<li>"+object.user+" - "+object.result+"/5</li>"); // add values to the list
+        });
+      }
+    
+      function handleScreens() {
+        $('#start-page').hide(); // hide start page
+        $('#question-page').hide(); // hide questions
+        $('#highscores-page').show(); // show scores
+      }
+    })
