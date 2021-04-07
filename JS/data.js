797c0217b1228570450a56ce66e9e0816f0b08c2
Jawascript <n.jmonplaisir@gmail.com>
2021-04-07 05:43:55 -0400

    adding questions

---
+++ JS/data.js
@@ -0,0 +1,52 @@
+var questions = [
+    {
+        title: 'What is the difference between a list and an array?',
+        choices: [
+        'an array is not iterable',
+        'they behave exactly the same',
+        'a list is an ordered group of items while an array is unordered',
+        'an array is called with parenthesis and list with curly brackets'
+        ],
+        answer: 'a list is an ordered group of items while an array is unordered'
+    },
+    {
+        title: 'What is a dynamic array?',
+        choices: [
+        'All arrays are dynamic',
+        'An array that automaticlaly resizes',
+        'An array which is predefined',
+        'An array which has multiple values'
+        ],
+        answer: 'An array that automaticlaly resizes'
+    },
+    {
+        title: 'What is the HTML tag for writing JavaScript?',
+        choices: [
+        '<JScript>',
+        '<java>',
+        '<script>',
+        '<js>'
+        ],
+        answer: '<script>'
+    },
+    {
+        title: 'Use ___ to verify if one value is NOT equal to another value',
+        choices: [
+        'not =',
+        '=!',
+        '!=',
+        '==='
+        ],
+        answer: '!='
+    },
+    {
+        title: "What Document method could you use to grab an element by it's id" ,
+        choices: [
+        'grabElement();',
+        'getElementById()',
+        'getElementsByTagName()',
+        "you can only grab an element by it's id using JQuery"
+        ],
+        answer: 'getElementById()'
+    },
+    ];
